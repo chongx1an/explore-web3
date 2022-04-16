ba63423a0a2f3c960f99facb7ab4606831d8fb4f
Chong Xian <chongxian@easystore.co>
2022-04-16 18:14:00 +0800

    add test

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,25 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("Lightsaber", function () {
+  it("Should pay to mint", async function () {
+    const Lightsaber = await ethers.getContractFactory("Lightsaber");
+    const lightsaber = await Lightsaber.deploy();
+    await lightsaber.deployed();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    const recipient = "0x8626f6940e2eb28930efb4cef49b2d1f2c9c1199";
+    const metadataURI = "cid/lightsaber.png";
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    let balance = await lightsaber.balanceOf(recipient);
+    expect(balance).to.equal(0);
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    const newToken = await lightsaber.payToMint(recipient, metadataURI, { value: ethers.utils.parseEther("0.05")});
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    await newToken.wait()
+
+    balance = await lightsaber.balanceOf(recipient);
+    expect(balance).to.equal(1);
+
+    expect(await lightsaber.isTokenOwned(metadataURI)).to.equal(true);
   });
 });
